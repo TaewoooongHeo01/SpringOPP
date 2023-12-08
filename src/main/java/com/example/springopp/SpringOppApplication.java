84d278d4dcd14a7524c43ac0e4e0d311d44fbd4d
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2023-12-08 16:35:50 +0900

    first commit

---
+++ src/main/java/com/example/springopp/SpringOppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springopp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springopp/SpringOppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springopp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
