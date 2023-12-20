6e4b0dc6bca4c5c1b0d20f5f5acf701673719d17
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2023-12-20 20:37:38 +0900

    Section3 OPP파트 완료

--- src/main/java/com/example/springopp/AppConfig.java
+++ src/main/java/com/example/springopp/AppConfig.java
@@ -1,7 +1,6 @@
 package com.example.springopp;
 
 import com.example.springopp.discount.DiscountPolicy;
-import com.example.springopp.discount.FixDiscountPolicy;
 import com.example.springopp.discount.RateDiscountPolicy;
 import com.example.springopp.member.MemberRepository;
 import com.example.springopp.member.MemberService;
@@ -9,21 +8,28 @@ import com.example.springopp.member.MemberServiceImpl;
 import com.example.springopp.member.MemoryMemberRepository;
 import com.example.springopp.order.OrderService;
 import com.example.springopp.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
---
+++ src/main/java/com/example/springopp/order/OrderApp.java
@@ -0,0 +1,26 @@
+package com.example.springopp.order;
+
+import com.example.springopp.AppConfig;
+import com.example.springopp.member.Grade;
+import com.example.springopp.member.Member;
+import com.example.springopp.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class OrderApp {
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order: "+order);
+
+    }
+}
